4aff42a68c4a2de6b9371ee426819e951bc26d59
Rantoryu <dominikmigrala@gmail.com>
2021-07-16 15:20:29 +0200

    First project in django framework

---
+++ firstApp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+#
+# class UploadFileForm(forms.Form):
+#     title = forms.CharField(max_length=50)
+#     file = forms.FileField()
+
+
+# class FileForm(forms.ModelForm):
+#     class Meta:
+#         model= File
+#         fields= ["name", "filepath"]
